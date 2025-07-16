4ae2a4aee1df85c83216e92f90ed505f51d524e8
Brambler <brambler.tools@gmail.com>
2025-07-15 21:02:38 -0400

    Initial GOTR Tracker Commit

--- src/main/java/com/gotrrunetracker/GOTRuneTrackerConfig.java
+++ src/main/java/com/gotrrunetracker/GOTRuneTrackerConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.gotrrunetracker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface GOTRuneTrackerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
@@ -14,6 +14,6 @@ public interface ExampleConfig extends Config
 	)
 	default String greeting()
 	{
-		return "Hello";
+		return "Runes";
 	}
 }
