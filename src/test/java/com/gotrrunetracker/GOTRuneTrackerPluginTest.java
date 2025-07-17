f1892e3ced9db21358ea4dd1f50bd51551159861
Brambler <jbb0020@mail.wvu.edu>
2025-07-17 14:07:51 -0400

    Renamed Test

--- src/test/java/com/gotrrunetracker/GOTRuneTrackerPluginTest.java
+++ src/test/java/com/gotrrunetracker/GOTRuneTrackerPluginTest.java
@@ -3,7 +3,7 @@ package com.gotrrunetracker;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GOTRuneTrackerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
